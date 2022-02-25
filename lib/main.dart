e4af0146385cf33286ee6c1e3319aea04a4fa68d
miguel <miguel.tavares@caixamagica.pt>
2022-02-25 12:14:04 +0000

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key key, this.title}) : super(key: key);
+  HomeScreen({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
